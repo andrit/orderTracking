b218de7fcbdd71019ecfe7e3d1b8639ac18f48b5
Andrew Ritter <andrit@users.noreply.github.com>
2018-10-06 17:30:44 -0400

    Update ajaxroutes.js

--- ui/src/utilities/ajaxroutes.js
+++ ui/src/utilities/ajaxroutes.js
@@ -1,7 +1,7 @@
 let myHeaders = new Headers();
 
-export const invoicelookuproute = 'https://apps.pcrichard.com:8082/www/invoice/exists.php';
-export const deliverydataroute = 'https://apps.pcrichard.com:8082/www/invoice/getData.php';
+export const invoicelookuproute = 'https://url.com/invoice/exists.php';
+export const deliverydataroute = 'https://url.com/invoice/getData.php';
 
 export const postCorsResObj = { method: 'POST',
                      header:myHeaders,
